cd8b75e1a83df1ba6a52c120bf36a0298058fd33
Sebastian Mortag <sebastian@mortag.com>
2025-08-07 15:29:05 +0200

    refactor matlab function call to use dynamic function name

--- moo_sim_interface/simulation_environment_apis/matlab_simulator.py
+++ moo_sim_interface/simulation_environment_apis/matlab_simulator.py
@@ -18,11 +18,15 @@ def run_simulation(return_results: bool = False, **args) -> Union[None, list]:
     model_path = Path(model_file).stem
     model_name = args.get('model_name')
 
+    function_name = Path(model_file).name
+
     n_outputs = len(args.get('simulation_setup').get('output_configuration').get('parameter_names'))
 
     eng = matlab.engine.start_matlab()
     path = eng.genpath(model_path)
     eng.addpath(path, nargout=0)
+
+    matlab_function = getattr(eng, function_name)
 
     indices = list(np.ndindex(input_values[0].shape if len(input_values) > 0 else (1,)))
 
@@ -31,7 +35,7 @@ def run_simulation(return_results: bool = False, **args) -> Union[None, list]:
         initial_values = [values[i] for values in input_values]  # set the start values
 
         # TODO: make the call generic
-        results = eng.Household_PV_BESS_example(*[matlab.double(val) for val in initial_values], nargout=n_outputs)
+        results = matlab_function(*[matlab.double(val) for val in initial_values], nargout=n_outputs)
         combined_results.append([(i, result_transformation(results))])
         combined_results.append([])  # placeholder for all parameters results
 
